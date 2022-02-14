e56d635636c1a21d992e017bd7770a4cab02f1c4
Victor Lopez <victorwkey@gmail.com>
2022-02-14 14:15:28 -0600

    V1, HTML and JS code finished, standard discount and coupon discount calculate finished

--- discounts.js
+++ discounts.js
@@ -1,4 +1,4 @@
-
+//OPERATION CODE
 
 function calculateDiscountedPrice(price, discount){
     const pricePercentageWithDiscount = 100 - discount;
@@ -6,6 +6,8 @@ function calculateDiscountedPrice(price, discount){
 
     return priceWithDiscount;
 }
+
+//DISCOUNT CODE
 
 function discountedPriceResult(){
     const inputPrice = document.getElementById("inputPrice");
@@ -20,5 +22,39 @@ function discountedPriceResult(){
     resultP.innerText = "The discounted price is $" + discountedPrice;
 }
 
+//COUPON DISCOUNT CODE
 
+
+function discountedCouponPriceResult(){
+    const coupons = ["youcan", "vicky", "cod"];
+
+    var discount;
+
+    const inputPrice2 = document.getElementById("inputPrice2");
+    const priceValue2 = inputPrice2.value;
+
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const couponP = document.getElementById("couponP");
+
+    if(!coupons.includes(couponValue)){
+        couponP.innerText = couponValue + " is an invalid coupon!";
+        discount = 0;
+    } else if (couponValue === coupons[0]) {
+        discount = 15;   
+        couponP.innerText = discount + "% discount applied";
+    } else if (couponValue === coupons[1]){
+        discount = 20;  
+        couponP.innerText = discount + "% discount applied";
+    } else if (couponValue === coupons[2]){
+        discount = 25;
+        couponP.innerText = discount + "% discount applied";
+    }
+
+    const discountedPrice2 = calculateDiscountedPrice(priceValue2, discount);
+
+    const resultP2 = document.getElementById("resultP2");
+    resultP2.innerText = "The discounted price is $" + discountedPrice2;
+}
 
